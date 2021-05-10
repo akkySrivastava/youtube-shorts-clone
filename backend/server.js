68a5a59262df836b6984f96770d43ad67a9b3567
akkySrivastava <akkyjaicar@gmail.com>
2021-05-10 20:52:47 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const router = require("./routers");
 const bodyParser = require("body-parser");
